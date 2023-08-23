476553b1ffd763bbf517c65ad2719e47063d9f07
Kalgoran <gilles.tran6@gmail.com>
2023-08-23 17:37:42 +0200

    change jump

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -8,10 +8,16 @@ public class PlayerController2D : MonoBehaviour
 {
     [SerializeField]
     private float speed = 25f;
+
     [SerializeField]
-    private float jumpForce = 25f;
+    private float jumpStartupSpeed = 20f;
     [SerializeField]
-    private float gravity = 3f;
+    private float jumpHeight = 4f;
+    [SerializeField]
+    private float jumpSpeedDecay = 0.1f;
+    [SerializeField]
+    private float jumpGravityMultiplier = 1.005f;
+
     [SerializeField]
     private float dashForce = 30f;
     [SerializeField]
@@ -19,7 +25,7 @@ public class PlayerController2D : MonoBehaviour
     [SerializeField]
     private float stopThresh = 0.3f;
 
-    private float facing = 1;
+    private float facing = 1f;
 
     private Rigidbody2D rb;
     private CapsuleCollider2D cl;
@@ -38,49 +44,43 @@ public class PlayerController2D : MonoBehaviour
     {
     }
 
+    private float height = -1f;
     // Update is called once per frame
     void Update()
     {
-        float vx;
-        float vy;
+        bool bOnGround = DetectGround();
 
-        bool a = DetectGround();
-
-        //jump
-        if (a && Input.GetButtonDown("Jump"))
+        // jump
+        // upward acceleration
+        if (bOnGround && Input.GetButtonDown("Jump"))
         {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); // add vertical force
+            height = transform.position.y;
+            rb.AddForce(Vector2.up * jumpStartupSpeed, ForceMode2D.Impulse);
         }
+        float vx = rb.velocity.x;
+        float vy = rb.velocity.y;
+        // acceleration decay
+        if (transform.position.y >= height + jumpHeight && vy > 0f)
+            vy *= 1f - jumpSpeedDecay;
+        // downward acceleration (gravity multiplier)
+        if (rb.velocity.y <= 0f)
+            vy *= jumpGravityMultiplier;
+        Mathf.Clamp(vy, -15f, speed);
 
-        //faster fall 
-        vy = rb.velocity.y;
-        if (rb.velocity.y < 0)
-        {
-            rb.gravityScale = gravity;
-        }
-        else
-        {
-            rb.gravityScale = 1f;
-        }
-        Mathf.Clamp(vy, -15, speed);
 
-        //s'accroupir
-        transform.localScale = new Vector3(1, 1f-Mathf.Abs(Input.GetAxis("Crouch"))/2f, 1);
+        // crouch
+        transform.localScale = new Vector3(1, 1f - Mathf.Abs(Input.GetAxis("Crouch")) / 2f, 1);
 
-        //right and left
-        if (a)
+
+        // right and left
+        if (bOnGround)
         {
             if (Input.GetAxisRaw("Horizontal") != 0)
-            {
                 facing = Input.GetAxisRaw("Horizontal");
-            }
             if (Mathf.Abs(Input.GetAxis("Horizontal")) > stopThresh)
-            {             
                 vx = Mathf.SmoothDamp(rb.velocity.x, facing * speed, ref vel, smoothTime);
-            } else
-            {
+            else
                 vx = Input.GetAxis("Horizontal") * speed; //faster stop
-            }           
         }
         else
         {
@@ -89,11 +89,11 @@ public class PlayerController2D : MonoBehaviour
         vx = Mathf.Clamp(vx, -speed, speed);
         rb.velocity = new Vector2(vx, vy);
 
-        //dash
+
+        // dash
         if (Input.GetButtonDown("Fire3"))
-        {
             rb.AddForce(Vector2.right * facing * dashForce, ForceMode2D.Impulse); // add horizontal force 
-        }
+        
 
         //facing
         transform.localScale = new Vector3(transform.localScale.x * facing,
@@ -103,7 +103,7 @@ public class PlayerController2D : MonoBehaviour
 
     bool DetectGround()
     {
-        return Physics2D.CircleCast(cl.bounds.center, cl.bounds.size.x/2, Vector2.down, cl.bounds.size.y/2);
+        return Physics2D.CircleCast(cl.bounds.center, cl.bounds.size.x / 2, Vector2.down, cl.bounds.size.y / 2);
     }
 
     public float GetFacing()
